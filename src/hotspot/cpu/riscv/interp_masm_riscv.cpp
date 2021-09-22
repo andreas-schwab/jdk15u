3fbf9fd52bddd06349377811f059d544edbaff3f
Andreas Schwab <schwab@linux-m68k.org>
2021-09-22 21:05:22 +0200

    4e43b28858b0c7c9fb7ad91a506b61aa1d554f86

--- src/hotspot/cpu/riscv/interp_masm_riscv.cpp
+++ src/hotspot/cpu/riscv/interp_masm_riscv.cpp
@@ -737,14 +737,14 @@ void InterpreterMacroAssembler::remove_activation(
 
   // remove activation
   // get sender esp
-  ld(t1,
+  ld(esp,
      Address(fp, frame::interpreter_frame_sender_sp_offset * wordSize));
   if (StackReservedPages > 0) {
     // testing if reserved zone needs to be re-enabled
     Label no_reserved_zone_enabling;
 
     ld(t0, Address(xthread, JavaThread::reserved_stack_activation_offset()));
-    ble(t1, t0, no_reserved_zone_enabling);
+    ble(esp, t0, no_reserved_zone_enabling);
 
     call_VM_leaf(
       CAST_FROM_FN_PTR(address, SharedRuntime::enable_stack_reserved_zone), xthread);
@@ -754,10 +754,6 @@ void InterpreterMacroAssembler::remove_activation(
 
     bind(no_reserved_zone_enabling);
   }
-
-  // restore sender esp
-  mv(esp, t1);
-
   // remove frame anchor
   leave();
   // If we're returning to interpreted code we will shortly be
