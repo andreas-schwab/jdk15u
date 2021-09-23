939c3d159afb5fe71b2c2a1aec7974c19b057541
Andreas Schwab <schwab@linux-m68k.org>
2021-09-23 16:03:11 +0200

    6bc493188b265b1f9776aa62f77c161d75fc591e

--- src/hotspot/cpu/riscv/templateInterpreterGenerator_riscv.cpp
+++ src/hotspot/cpu/riscv/templateInterpreterGenerator_riscv.cpp
@@ -948,7 +948,7 @@ void TemplateInterpreterGenerator::bang_stack_shadow_pages(bool native_call) {
   // needs to be checked.  Only true for non-native.
   if (UseStackBanging) {
     const int page_size = os::vm_page_size();
-    const int n_shadow_pages = ((int)StackOverflow::stack_shadow_zone_size()) / page_size;
+    const int n_shadow_pages = ((int)JavaThread::stack_shadow_zone_size()) / page_size;
     const int start_page = native_call ? n_shadow_pages : 1;
     for (int pages = start_page; pages <= n_shadow_pages; pages++) {
       __ bang_stack_with_offset(pages*page_size);
@@ -1241,7 +1241,7 @@ address TemplateInterpreterGenerator::generate_native_entry(bool synchronized) {
   {
     Label no_reguard;
     __ lwu(t0, Address(xthread, in_bytes(JavaThread::stack_guard_state_offset())));
-    __ addi(t1, zr, (u1)StackOverflow::stack_guard_yellow_reserved_disabled);
+    __ addi(t1, zr, (u1)JavaThread::stack_guard_yellow_reserved_disabled);
     __ bne(t0, t1, no_reguard);
 
     __ pusha(); // only save smashed registers
