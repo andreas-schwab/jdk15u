5ee55ee85754a865ad88ff86001c48b236bbd2c9
Andreas Schwab <schwab@linux-m68k.org>
2021-09-23 15:50:32 +0200

    9cecc167472338d3aa00f4730fa4e3472058bb52

--- src/hotspot/cpu/riscv64/templateTable_riscv64.cpp
+++ src/hotspot/cpu/riscv64/templateTable_riscv64.cpp
@@ -1864,7 +1864,7 @@ void TemplateTable::branch(bool is_jsr, bool is_wide)
   __ dispatch_only(vtos, /*generate_poll*/true);
 
   if (UseLoopCounter) {
-    if (ProfileInterpreter && !TieredCompilation) {
+    if (ProfileInterpreter) {
       // Out-of-line code to allocate method data oop.
       __ bind(profile_method);
       __ call_VM(noreg, CAST_FROM_FN_PTR(address, InterpreterRuntime::profile_method));
