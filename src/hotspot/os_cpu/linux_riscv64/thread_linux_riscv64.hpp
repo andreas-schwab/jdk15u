0905c6203ead66179aa67192f3546016d1af43aa
Andreas Schwab <schwab@linux-m68k.org>
2021-09-23 16:45:01 +0200

    b8ea80af33ecc9d4ccfa3c3dc366e1a88c3607b4

--- src/hotspot/os_cpu/linux_riscv64/thread_linux_riscv64.hpp
+++ src/hotspot/os_cpu/linux_riscv64/thread_linux_riscv64.hpp
@@ -48,8 +48,17 @@
   intptr_t* last_Java_fp()                       { return _anchor.last_Java_fp(); }
   void  set_last_Java_fp(intptr_t* fp)           { _anchor.set_last_Java_fp(fp);   }
 
+  void set_base_of_stack_pointer(intptr_t* base_sp) {
+  }
+
   static ByteSize last_Java_fp_offset()          {
     return byte_offset_of(JavaThread, _anchor) + JavaFrameAnchor::last_Java_fp_offset();
+  }
+
+  intptr_t* base_of_stack_pointer() {
+    return NULL;
+  }
+  void record_base_of_stack_pointer() {
   }
 
   bool pd_get_top_frame_for_signal_handler(frame* fr_addr, void* ucontext,
@@ -61,5 +70,11 @@ private:
 public:
 
   static Thread *riscv64_get_thread_helper();
+
+  // These routines are only used on cpu architectures that
+  // have separate register stacks (Itanium).
+  static bool register_stack_overflow() { return false; }
+  static void enable_register_stack_guard() {}
+  static void disable_register_stack_guard() {}
 
 #endif // OS_CPU_LINUX_RISCV64_THREAD_LINUX_RISCV64_HPP
